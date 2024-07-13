bf04a47a1281cf9e5d015396ee1cd42af1bdbb56
Tatyana Karpenko <Tatyanna_ktp@mail.ru>
2024-07-13 13:40:37 +1000

    Add modul utils.py with func get_transactions and file operations.json

---
+++ src/utils.py
@@ -0,0 +1,26 @@
+import json
+
+
+def get_transactions(path: str) -> list:
+    """Функцию, которая принимает на вход путь до JSON-файла и
+    возвращает список словарей с данными о финансовых транзакциях"""
+    try:
+        with open(path, "r", encoding="utf-8") as operations_file:
+            transactions = json.load(operations_file)
+
+            if isinstance(transactions, list):
+                return transactions
+            else:
+                return []
+
+    except json.JSONDecodeError:
+        print("Ошибка декодирования файла")
+        return []
+    except FileNotFoundError:
+        print("Файл не найден")
+        return []
+
+
+if __name__ == "__main__":
+    path = "../data/operations.json"
+    print(get_transactions(path))
